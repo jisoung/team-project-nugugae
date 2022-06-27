e086b7ddd575336ba176ba0b3a61210eb1f6ca6d
jisoung <jangjisoung0405@gmail.com>
2022-06-27 14:03:05 +0900

    commit

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const brownTheme: DefaultTheme = {
+  color: "#87805E",
+  color2: "#B09B71",
+  color3: "#D8CCA3",
+  color4: "#EDDFB3",
+};
