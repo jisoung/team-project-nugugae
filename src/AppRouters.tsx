984be4f7e47389b3ce65ab3116804b30ad01400e
jisoung <jangjisoung0405@gmail.com>
2022-06-29 17:31:01 +0900

    commit

--- src/AppRouters.tsx
+++ src/AppRouters.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import LogInHomePage from "./Components/LogIn/LogInHomePage";
 import LogInPage from "./Components/LogIn/LogInPage";
 import SignUpPage from "./Components/LogIn/SignUpPage";
+import HomePage from "./Components/Main/HomePage";
 
 function AppRouters() {
   return (
@@ -10,6 +11,7 @@ function AppRouters() {
         <Route path="/" element={<LogInHomePage />} />
         <Route path="/login" element={<LogInPage />} />
         <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
