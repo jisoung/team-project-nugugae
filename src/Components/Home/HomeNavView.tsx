21bb91bcdf9d79b8eba0bf8f480439609afcf0e4
jisoung <jangjisoung0405@gmail.com>
2022-06-30 23:30:54 +0900

    commit

--- src/Components/Home/HomeNavView.tsx
+++ src/Components/Home/HomeNavView.tsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import TitleLogo from "../LogIn/btns/titleLogo";
-
 const NavContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const Img = styled.img`
+  position: absolute;
 `;
 
 const CtgText = styled.button`
@@ -13,11 +16,20 @@ const CtgText = styled.button`
   border: 0;
   background-color: #ffffff;
 `;
+
+const Search = styled.input`
+  border: 0;
+  border-bottom: 1px white;
+`;
 function HomeNavView() {
   return (
     <NavContainer>
+      <Img src={require("../Home/img")} />
       <TitleLogo lhaveText={false} lwidth={52} lheight={52} />
       <CtgText>FIND</CtgText>
+      <CtgText>ADOPT</CtgText>
+      <CtgText>SHOP</CtgText>
+      <Search />
     </NavContainer>
   );
 }
