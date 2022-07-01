853c62cafcb5fa3d9bcc4426f0dfe931109b53d0
jisoung <jangjisoung0405@gmail.com>
2022-07-01 20:19:33 +0900

    Add bakcground Img

--- src/MainPages/HomeMainPage.tsx
+++ src/MainPages/HomeMainPage.tsx
@@ -3,14 +3,21 @@ import HomeNavPage from "../Components/Home/HomeNavView";
 import HomeAdoptView from "../Components/Home/option/HomeAdoptPage";
 import HomeFindPage from "../Components/Home/option/HomeFindView";
 import HomeShopPage from "../Components/Home/option/HomeShopView";
-
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
+  background-image: url("img/backgroundImg.webp");
+  background-size: 100vw 100vh;
+  width: 100vw;
+  height: 100vh;
 `;
-function HomePage() {
+const Img = styled.image`
+  position: absolute;
+`;
+function HomeMainPage() {
   return (
     <MainContainer>
+      <Img />
       <HomeNavPage />
       <HomeFindPage />
       <HomeAdoptView />
@@ -19,4 +26,4 @@ function HomePage() {
   );
 }
 
-export default HomePage;
+export default HomeMainPage;
