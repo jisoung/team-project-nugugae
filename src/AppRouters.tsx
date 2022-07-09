fd9cd25eb58057a5e827b3ddebb059df5f51319c
jisoung <jangjisoung0405@gmail.com>
2022-07-09 18:15:51 +0900

    fix

--- src/AppRouters.tsx
+++ src/AppRouters.tsx
@@ -3,16 +3,18 @@ import LogInHomePage from "./MainPages/LogInHomePage";
 import LogInPage from "./Components/LogIn/LogInPage";
 import SignUpPage from "./Components/LogIn/SignUpPage";
 import HomePage from "./MainPages/HomeMainPage";
+import FindHomePage from "./MainPages/Detail/FindHomePage";
 
 function AppRouters() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/loginHome" element={<LogInHomePage />} />
         <Route path="/loginHome/login" element={<LogInPage />} />
         <Route path="/loginHome/signUp" element={<SignUpPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/find" element={<FindHomePage />} />
       </Routes>
     </BrowserRouter>
   );
