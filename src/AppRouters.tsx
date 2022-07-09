fe55f063559340c1cfc53e10b171fa074126b571
jisoung <jangjisoung0405@gmail.com>
2022-07-09 17:37:03 +0900

    commit

--- src/AppRouters.tsx
+++ src/AppRouters.tsx
@@ -8,9 +8,10 @@ function AppRouters() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LogInHomePage />} />
-        <Route path="/login" element={<LogInPage />} />
-        <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/loginHome" element={<LogInHomePage />} />
+        <Route path="/loginHome/login" element={<LogInPage />} />
+        <Route path="/loginHome/signUp" element={<SignUpPage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
