ec8f8697f3153b94deb8fc8b48cabfb287bad41f
jisoung <jangjisoung0405@gmail.com>
2022-07-09 19:24:39 +0900

    commit

--- src/Components/Detail/Find/FindNavView.tsx
+++ src/Components/Detail/Find/FindNavView.tsx
@@ -1,5 +1,27 @@
+import * as S from "../../../Styles/Detail/Find/FindNavStyle";
+import TitleLogo from "../../LogIn/titleLogo";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function FindNavView() {
-  return <h1>find nav</h1>;
+  const profileStyle = {
+    width: "26px",
+    height: "26px",
+    marginRight: "30px",
+    cursor: "pointer",
+  };
+  return (
+    <S.NavContainer>
+      <TitleLogo lwidth={30} lheight={30} lhaveText={false} fontSize="30px" />
+      <div>
+        <FontAwesomeIcon
+          icon={faUser}
+          color={"rgba(0, 0, 0, 0.5)"}
+          style={profileStyle}
+        />
+      </div>
+    </S.NavContainer>
+  );
 }
 
 export default FindNavView;
---
+++ src/Styles/Detail/Find/FindNavStyle.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-top: 40px;
+`;
