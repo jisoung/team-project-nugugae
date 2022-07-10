40f2da14ed291367f62d31eb60b76075decca1fa
jisoung <jangjisoung0405@gmail.com>
2022-07-10 23:32:48 +0900

    fix

---
+++ src/Styles/Detail/DetailNavStyle.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-top: 40px;
+`;
