33cec93b4e623a4b9e53d7bbe350253b699b6438
jisoung <jangjisoung0405@gmail.com>
2022-07-10 23:32:56 +0900

    .

--- src/Styles/Detail/Find/FindNavStyle.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const NavContainer = styled.div`
-  display: flex;
-  width: 100vw;
-  justify-content: space-between;
-  align-items: flex-start;
-  margin-top: 40px;
-`;
