02d58958fb97935655c5ef00b8100927bafc4a97
jisoung <jangjisoung0405@gmail.com>
2022-07-11 17:35:54 +0900

    fix

--- src/Components/Detail/sample/SmpNavView.tsx
+++ src/Components/Detail/sample/SmpNavView.tsx
@@ -1,4 +1,4 @@
-import * as S from "../../../Styles/Detail/DetailNavStyle";
+import * as S from "../../../Styles/Detail/SmpNavStyle";
 import TitleLogo from "../../LogIn/titleLogo";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,12 +7,12 @@ function FindNavView() {
   const profileStyle = {
     width: "26px",
     height: "26px",
-    marginRight: "80px",
+    marginRight: "100px",
     cursor: "pointer",
   };
   return (
     <S.NavContainer>
-      <div style={{ margin: "0px 0px 0px 80px" }}>
+      <div style={{ margin: "0px 0px 0px 100px" }}>
         <TitleLogo lwidth={30} lheight={30} lhaveText={false} fontSize="30px" />
       </div>
       <div>
