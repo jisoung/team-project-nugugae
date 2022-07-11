ee053f4440901256783b5e124a09acf195809830
jisoung <jangjisoung0405@gmail.com>
2022-07-11 19:14:51 +0900

    Feat Link to home at titleIcon

--- src/Components/Detail/sample/SmpNavView.tsx
+++ src/Components/Detail/sample/SmpNavView.tsx
@@ -2,7 +2,6 @@ import * as S from "../../../Styles/Detail/SmpNavStyle";
 import TitleLogo from "../../LogIn/titleLogo";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import * as S2 from "../../../Styles/Home/HomeNavStyle";
 import { Link } from "react-router-dom";
 
 function FindNavView() {
@@ -10,24 +9,26 @@ function FindNavView() {
     width: "26px",
     height: "26px",
     marginRight: "100px",
+    marginLeft: "100px",
     cursor: "pointer",
   };
   return (
     <S.NavContainer>
-      <div style={{ margin: "0px 0px 0px 100px" }}>
-        <TitleLogo lwidth={30} lheight={30} lhaveText={false} fontSize="30px" />
-      </div>
-      <S2.CtgTexts>
-        <S2.CtgText>
-          <Link to="find">FIND</Link>
-        </S2.CtgText>
-        <S2.CtgText>
-          <Link to="adopt">ADOPT</Link>
-        </S2.CtgText>
-        <S2.CtgText>
-          <Link to="shop">SHOP</Link>
-        </S2.CtgText>
-      </S2.CtgTexts>
+      <Link to="/">
+        <div style={{ margin: "0px 0px 0px 100px" }}>
+          <TitleLogo
+            lwidth={30}
+            lheight={30}
+            lhaveText={false}
+            fontSize="30px"
+          />
+        </div>
+      </Link>
+      <S.CtgTexts>
+        <S.CtgText>FIND</S.CtgText>
+        <S.CtgText>ADOPT</S.CtgText>
+        <S.CtgText>SHOP</S.CtgText>
+      </S.CtgTexts>
       <div>
         <FontAwesomeIcon
           icon={faUser}
