a701095547c012475f0a07feb880ad701d5406fa
jisoung <jangjisoung0405@gmail.com>
2022-07-11 17:36:04 +0900

    Add side view

--- src/Components/Detail/sample/SmpSideView.tsx
+++ src/Components/Detail/sample/SmpSideView.tsx
@@ -1,13 +1,42 @@
-import { SideContainer } from "../../../Styles/Detail/SmpSideStyle";
-
+import { useState } from "react";
+import * as S from "../../../Styles/Detail/SmpSideStyle";
 interface ISideProps {
-  title?: string;
+  title: string;
 }
+
 function SmpSideView({ title }: ISideProps) {
+  const [isMenuOn, setIsMenuOn] = useState(false);
+  const onClick = () => {
+    setIsMenuOn((current) => !current);
+  };
   return (
-    <SideContainer>
-      <h1>{title}</h1>
-    </SideContainer>
+    <S.SideContainer>
+      <S.Title>{title}</S.Title>
+      <S.Category>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "10px",
+          }}
+        >
+          <h2>CATEGORY</h2>
+          <S.CategoryBtn onClick={onClick}>-</S.CategoryBtn>
+        </div>
+        <S.CategoryUl>
+          <S.CategoryLi menuOn={isMenuOn} delay={0}>
+            DOG
+          </S.CategoryLi>
+          <S.CategoryLi menuOn={isMenuOn} delay={0.15}>
+            CAT
+          </S.CategoryLi>
+          <S.CategoryLi menuOn={isMenuOn} delay={0.3}>
+            OTHER
+          </S.CategoryLi>
+        </S.CategoryUl>
+      </S.Category>
+    </S.SideContainer>
   );
 }
 
