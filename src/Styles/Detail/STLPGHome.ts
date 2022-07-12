0ab2b8b366d410cea0d501cd7cdf14e9286a54d2
jisoung <jangjisoung0405@gmail.com>
2022-07-12 18:29:50 +0900

    Feat module style about PGSmpHome

---
+++ src/Styles/Detail/STLPGHome.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SmpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const SmpMainContainer = styled.div`
+  height: 50vh;
+  display: flex;
+  align-items: center;
+`;
