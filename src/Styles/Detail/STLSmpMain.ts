baa8073759e4f3294f73b9727f47e896300b6569
jisoung <jangjisoung0405@gmail.com>
2022-07-12 18:29:54 +0900

    fix

---
+++ src/Styles/Detail/STLSmpMain.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SmpMainContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 50vh;
+  border: 1px solid;
+`;
