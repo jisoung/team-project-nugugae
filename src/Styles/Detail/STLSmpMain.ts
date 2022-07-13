bb8c8989994540d15def7ae7424129377262b1cf
jisoung <jangjisoung0405@gmail.com>
2022-07-13 12:17:58 +0900

    Feat SmpMainStyle

--- src/Styles/Detail/STLSmpMain.ts
+++ src/Styles/Detail/STLSmpMain.ts
@@ -2,9 +2,17 @@ import styled from "styled-components";
 
 export const SmpMainContainer = styled.div`
   display: grid;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 50vh;
-  border: 1px solid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 60px;
+  width: 1300px;
+  margin-top: 60px;
+  margin-right: 100px;
+  padding: 0 40px 0 40px;
+  align-content: center;
+`;
+export const Bundle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 5px;
+  box-shadow: 0 1px 10px 1px rgba(0, 0, 0, 0.1);
 `;
