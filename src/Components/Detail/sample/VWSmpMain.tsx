6e0ab965f7ec89d314a41078db142ef09282a3e2
jisoung <jangjisoung0405@gmail.com>
2022-07-13 12:17:44 +0900

    feat VWSmpMain

--- src/Components/Detail/sample/VWSmpMain.tsx
+++ src/Components/Detail/sample/VWSmpMain.tsx
@@ -1,7 +1,22 @@
+import { Link, useLocation } from "react-router-dom";
 import * as S from "../../../STYLES/Detail/STLSmpMain";
 
 function VWSmpMain() {
-  return <S.SmpMainContainer></S.SmpMainContainer>;
+  const location = useLocation();
+  console.log(location);
+  const bundles = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+  return (
+    <S.SmpMainContainer>
+      {bundles.map((bundle) => (
+        <Link to="animal">
+          <S.Bundle>{bundle}</S.Bundle>
+          <p>{location.search}</p>
+        </Link>
+      ))}
+    </S.SmpMainContainer>
+  );
 }
 
 export default VWSmpMain;
