6b080dbb93d10c5e6ae194a56cecaced65d7ba0f
jisoung <jangjisoung0405@gmail.com>
2022-07-21 22:34:10 +0900

    .

--- src/Components/Detail/sample/VWSmpMain.tsx
+++ src/Components/Detail/sample/VWSmpMain.tsx
@@ -1,18 +1,38 @@
-import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { tokenState } from "../../../atom";
 import * as S from "../../../STYLES/Detail/STLSmpMain";
 
+interface IBundle {}
 function VWSmpMain() {
-  const location = useLocation();
-  console.log(location);
-  const bundles = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+  const ascToken = useRecoilState(tokenState);
+  const [bundles, setBundles] = useState([]);
+  const [kind, setKind] = useState(1);
+  const [page, setPage] = useState(1);
+  console.log(ascToken[0]);
+  let config = {
+    method: "get",
+    url: `http://192.168.192.253:8080/api/pet/search?s=${kind}&p=${page}`,
+    headers: {
+      Authorization: `Bearer ${ascToken[0]}`,
+    },
+    maxRedirects: 0,
+  };
+  axios(config)
+    .then((response) => {
+      console.log(response.data);
+      setBundles(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   return (
     <S.SmpMainContainer>
       {bundles.map((bundle) => (
         <Link to="animal">
           <S.Bundle>{bundle}</S.Bundle>
-          <p>{location.search}</p>
         </Link>
       ))}
     </S.SmpMainContainer>
