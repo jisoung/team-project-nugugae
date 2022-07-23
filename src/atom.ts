f9590dfce21d849d1ffaac128f9b195cfcf324b5
jisoung <jangjisoung0405@gmail.com>
2022-07-23 23:31:45 +0900

    Fix - atom

--- src/atom.ts
+++ src/atom.ts
@@ -4,7 +4,16 @@ export interface IToken {
   ascToken: string;
 }
 
+export interface IUrl {
+  default: string;
+}
+
 export const tokenState = atom<IToken[]>({
   key: "token",
   default: [],
+});
+
+export const localUrl = atom<IUrl>({
+  key: "localUrl",
+  default: "https://967d-222-118-155-166.jp.ngrok.io",
 });
