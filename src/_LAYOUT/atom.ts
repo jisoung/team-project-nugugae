1f6e7cd0b0fdb3b2bb8dd3f9ec607a44f6625f28
jisoung <jangjisoung0405@gmail.com>
2022-07-29 22:22:32 +0900

    .

--- src/_LAYOUT/atom.ts
+++ src/_LAYOUT/atom.ts
@@ -1,16 +1,12 @@
 import { atom } from "recoil";
 
-export interface IToken {
-  ascToken: string;
-}
-
 export const isLogin = atom<boolean>({
   key: "isLogin",
   default: false,
 });
 
-export const tokenState = atom<IToken[]>({
+export const tokenState = atom<string>({
   key: "token",
-  default: [],
+  default: "",
 });
 
